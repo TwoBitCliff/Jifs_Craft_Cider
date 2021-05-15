4aa30106ade76bcf3db3622b5536e680dce82892
TwoBitCliff <j-lithgow@live.co.uk>
2021-05-15 13:41:37 +0000

    Fixed products fixture error

--- products/models.py
+++ products/models.py
@@ -18,16 +18,16 @@ class Category(models.Model):
         return self.friendly_name
 
 
-class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    product_id = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
-    brand = models.CharField(max_length=254)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    quantity = models.CharField(max_length=254)    
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+class Product(models.Model):    
     image = models.ImageField(null=True, blank=True)
-
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    product_id = models.CharField(max_length=254, null=True, blank=True)    
+    brand = models.CharField(max_length=254)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    quantity = models.CharField(max_length=254) 
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
     def __str__(self):
         return self.name
