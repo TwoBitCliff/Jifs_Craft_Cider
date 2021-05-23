4e8099d41b8c95b91302cce54d0e125841fe24a6
TwoBitCliff <j-lithgow@live.co.uk>
2021-05-23 17:03:39 +0000

    Created edit product functionality

--- products/views.py
+++ products/views.py
@@ -70,7 +70,7 @@ def product_detail(request, product_id):
 
 
 def add_product(request):
-    """ Add a product to the store """
+    """  Add a product to the store  """
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -85,6 +85,28 @@ def add_product(request):
     template = 'products/add_product.html'
     context = {
         'form': form,
+    }
+
+
+def edit_product(request, product_id):
+    """  Edit a product in the store  """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product updated')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Ensure all information is correct.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
