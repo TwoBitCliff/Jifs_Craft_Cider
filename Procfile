1d2f4dc81b94236fc66aa4bb13b3e2bf4d224c6a
TwoBitCliff <j-lithgow@live.co.uk>
2021-05-25 17:59:19 +0000

    Further Heroku Deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jifs_craft_cider.wsgi:application
--- jifs_craft_cider/settings.py
+++ jifs_craft_cider/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-*g&a=w4%hj))vav7so-!-4rn2#iqx0dxgwegk+qdulyc@+qss9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jifs-craft-cider.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,17 +118,17 @@ WSGI_APPLICATION = 'jifs_craft_cider.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    'default': dj_database_url.parse('postgres://mlckrzshcicgqy:1ec7c5436f3fcfbb1c21d65cc4ccd0e3cd23ca65fe1d81e48bb4e1eb854b608d@ec2-52-209-134-160.eu-west-1.compute.amazonaws.com:5432/d8vlfgd1gj1b6i')
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
