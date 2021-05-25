6ad24b300d6c4bb7b74dd0716e06aae0d3eee7b1
TwoBitCliff <j-lithgow@live.co.uk>
2021-05-25 17:49:02 +0000

    Initial Heroku deployment

--- jifs_craft_cider/settings.py
+++ jifs_craft_cider/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,11 +119,15 @@ WSGI_APPLICATION = 'jifs_craft_cider.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://mlckrzshcicgqy:1ec7c5436f3fcfbb1c21d65cc4ccd0e3cd23ca65fe1d81e48bb4e1eb854b608d@ec2-52-209-134-160.eu-west-1.compute.amazonaws.com:5432/d8vlfgd1gj1b6i')
 }
 
 
