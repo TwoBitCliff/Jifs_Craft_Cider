6aaf56bb1ae95b7d0b2eb08d5e37227886b546c6
TwoBitCliff <j-lithgow@live.co.uk>
2021-05-25 17:13:06 +0000

    Revert "Further deployment to Heroku"
    This reverts commit 0ef631de450bbc476657c78bdae53efde3f8c746.

--- jifs_craft_cider/settings.py
+++ jifs_craft_cider/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-*g&a=w4%hj))vav7so-!-4rn2#iqx0dxgwegk+qdulyc@+qss9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jifs-craft-cider.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = []
+
 
 # Application definition
 
@@ -118,17 +119,12 @@ WSGI_APPLICATION = 'jifs_craft_cider.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
