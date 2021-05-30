716aaaa8586aa8886ea0a0c370d7429708c03d22
TwoBitCliff <j-lithgow@live.co.uk>
2021-05-30 12:14:31 +0000

    Created add to wishlist function

--- wishlist/contexts.py
+++ wishlist/contexts.py
@@ -5,17 +5,24 @@ from products.models import Product
 def wishlist_contents(request):
 
     wishlist_items = []
+    total = 0
+    product_count = 0
     wishlist = request.session.get('wishlist', {})
 
-    for item_id in wishlist.items():
+    for item_id, quantity in wishlist.items():
         product = get_object_or_404(Product, pk=item_id)
+        total += quantity
+        product_count += quantity
         wishlist_items.append({
             'item_id': item_id,
+            'quantity': quantity,
             'product': product,
         })
 
     context = {
         'wishlist_items': wishlist_items,
+        'total': total,
+        'product_count': product_count,
     }
 
     return context
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,20 @@ def view_wishlist(request):
     """ A view that renders the user wishlist """
 
     return render(request, 'wishlist/wishlist.html')
+
+
+def add_to_wishlist(request, item_id):
+    """ Add an item to the wishlist """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    wishlist = request.session.get('wishlist', {})
+
+    if item_id in list(wishlist.keys()):
+        wishlist[item_id] = quantity
+    else:
+        wishlist[item_id] = 1
+
+    request.session['wishlist'] = wishlist
+    print(request.session['wishlist'])
+    return redirect(redirect_url)
