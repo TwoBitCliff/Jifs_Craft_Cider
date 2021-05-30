8593c60edc1700b634569fd7e6b0172b06127696
TwoBitCliff <j-lithgow@live.co.uk>
2021-05-30 17:53:14 +0000

    Revert "Created review model, form and views"
    This reverts commit 502cf0f8c2573d40d128fe45080c5d021c3c47ab.

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, Reviews
+from .models import Product, Category
 
 # Register your models here.
 
@@ -27,14 +27,3 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
-
-
-@admin.register(Reviews)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('user_profile', 'product_id', 'body', 'created_on', 'active')
-    list_filter = ('created_on', 'active')
-    search_fields = ('user_profile', 'body')
-    actions = ['approve_comments']
-
-    def approve_comments(self, request, queryset):
-        queryset.update(active=True)
