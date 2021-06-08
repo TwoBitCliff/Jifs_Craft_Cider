e6a4c464584b1d6f9c837f8a8a056a15761c5a36
TwoBitCliff <j-lithgow@live.co.uk>
2021-06-08 20:44:04 +0000

    Fixed flake8 errors

--- bag/views.py
+++ bag/views.py
@@ -8,25 +8,25 @@ from products.models import Product
 def view_bag(request):
     """ A view that renders the bag contents page """
 
-    return render(request, 'bag/bag.html')
+    return render(request, "bag/bag.html")
 
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
+    quantity = int(request.POST.get("quantity"))
+    redirect_url = request.POST.get("redirect_url")
+    bag = request.session.get("bag", {})
 
     if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-        messages.success(request, f'{product.name} updated.')
+        bag[item_id] = quantity
+        messages.success(request, f"{product.name} updated.")
     else:
         bag[item_id] = quantity
-        messages.success(request, f'{product.name} added.')
+        messages.success(request, f"{product.name} added.")
 
-    request.session['bag'] = bag
+    request.session["bag"] = bag
     return redirect(redirect_url)
 
 
@@ -34,18 +34,18 @@ def adjust_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    bag = request.session.get('bag', {})
+    quantity = int(request.POST.get("quantity"))
+    bag = request.session.get("bag", {})
 
     if quantity > 0:
         bag[item_id] = quantity
-        messages.success(request, f'{product.name} updated.')
+        messages.success(request, f"{product.name} updated.")
     else:
         bag.pop(item_id)
-        messages.success(request, f'{product.name} removed')
+        messages.success(request, f"{product.name} removed")
 
-    request.session['bag'] = bag
-    return redirect(reverse('view_bag'))
+    request.session["bag"] = bag
+    return redirect(reverse("view_bag"))
 
 
 def remove_from_bag(request, item_id):
@@ -53,14 +53,14 @@ def remove_from_bag(request, item_id):
 
     try:
         product = get_object_or_404(Product, pk=item_id)
-        bag = request.session.get('bag', {})
+        bag = request.session.get("bag", {})
 
         bag.pop(item_id)
-        messages.success(request, f'{product.name} removed.')
+        messages.success(request, f"{product.name} removed.")
 
-        request.session['bag'] = bag
+        request.session["bag"] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
-        messages.error(request, f'Error removing item: (e)')
+        messages.error(request, f"Error removing item: (e)")
         return HttpResponse(status=500)
